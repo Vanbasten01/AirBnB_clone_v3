6b47ec0dfdd89c8a40a7dd24c0b5e07eeb5f1a97
Vanbasten01 <fouad.yasin01@gmail.com>
2023-10-27 19:14:42 +0100

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,21 @@ from flask import jsonify
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                   "users": storage.count(User)})
