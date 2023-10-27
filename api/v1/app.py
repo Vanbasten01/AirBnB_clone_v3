6add0b4b65abc81b719b5697d98900dd59123b33
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-27 21:13:54 +0100

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,12 @@ def close(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found_404(error):
+    '''Handle The error 404'''
+    return jsonify(error='Not found'), 404
+
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
