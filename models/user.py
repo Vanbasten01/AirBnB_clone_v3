30009931734fc931d962c10ced011a10f5c16f66
Vanbasten01 <fouad.yasin01@gmail.com>
2023-10-29 02:29:03 +0100

    tst

--- models/user.py
+++ models/user.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """ holds class User"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
+
+import hashlib
+
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    """Representation of a user"""
+
+    if models.storage_t == "db":
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -27,3 +29,13 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """password's Getter"""
+        return self.__password
+
+    @password.setter
+    def password(self, pwd):
+        """Setter for password"""
+        self.__password = hashlib.md5(pwd.encode()).hexdigest()
